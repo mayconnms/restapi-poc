085ca240299b777c4c0296943c7abff12666855a
Maycon <mayconn.ms@gmail.com>
2018-11-22 18:03:52 -0200

    Add security OAuth

---
+++ src/main/java/br/com/gjc/restapi/restapipoc/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package br.com.gjc.restapi.restapipoc.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class SecurityConfig {
+
+}
